8c28dac40ebf68505b6447e9c448ce870d984e65
HP2706 <hp2706@gmail.com>
2024-09-21 19:30:52 +0000

    .

--- setup.bash
+++ setup.bash
@@ -6,4 +6,33 @@ source ~/.bashrc
 pip install uv
 uv pip install -e .
 
-export WANDB_API_KEY='a3469eb2df23f67e4d6907ebacf50ffb4ee664f7' 
+# Add environment variables to ~/.bashrc
+echo '
+# Environment variables
+export WANDB_API_KEY="a3469eb2df23f67e4d6907ebacf50ffb4ee664f7"
+export HUGGINGFACE_API_KEY="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"
+export HF_TOKEN="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"
+export HF_HUB_ENABLE_HF_TRANSFER=1
+' >> ~/.bashrc
+
+# Source the updated .bashrc
+source ~/.bashrc
+
+# Create a jupyter kernel that includes these environment variables
+python -m ipykernel install --user --name=nlp_course_env
+
+# Create a jupyter config file if it doesn't exist
+jupyter notebook --generate-config
+
+# Add code to load environment variables in Jupyter
+echo "
+import os
+os.environ['WANDB_API_KEY'] = 'a3469eb2df23f67e4d6907ebacf50ffb4ee664f7'
+os.environ['HUGGINGFACE_API_KEY'] = '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'
+os.environ['HF_TOKEN'] = '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'
+os.environ['HF_HUB_ENABLE_HF_TRANSFER'] = '1'
+" >> ~/.ipython/profile_default/startup/load_env.py
+
+# Restart the Jupyter server
+jupyter notebook stop
+jupyter notebook
